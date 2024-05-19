3f1aa559843360305ac5c4714c6bc6a123ea0638
chiefllewellyn12 <llewellynnyarko@gmail.com>
2024-05-19 20:26:49 +0000

    Update arrayManipulatio.js

--- arrayManipulatio.js
+++ arrayManipulatio.js
@@ -29,6 +29,18 @@ function formatArrayStrings(array_strings,processedArray){
         }
     });
 
+    const { processArray, formatArrayStrings } = require('./arrayManipulation');
+const { createUserProfile } = require('./userInfo');
+
+const numbers = [1, 2, 3, 4, 5];
+const strings = ['my', 'first', 'JavaScript', 'is', 'awesome'];
+
+const processedNumbers = processArray(numbers);
+const formattedStrings = formatArrayStrings(strings, processedNumbers);
+const userProfiles = createUserProfile(strings, formattedStrings);
+
+console.log(userProfiles);
+
 }
 
 
